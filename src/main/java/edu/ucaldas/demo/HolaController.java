e0fb1f49420b955e297014a4606ae107ebd12bf2
santiago <osoriosantiago486@gmail.com>
2026-04-05 07:45:11 -0500

    primer commit

---
+++ src/main/java/edu/ucaldas/demo/HolaController.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/")
+    public String hola() {
+        return "Hola mundo desde Spring Boot 🚀";
+    }
+}
